4345f55a0cf3ecfc8a1f4138aa37311f35445ec6
= <=>
2020-08-04 08:22:39 -1200

    Initial commit

---
+++ src/views/DynamicForm.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container-fl">
+      <div class="container">
+          <h1>Welcome to dynamic forms</h1>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+
+}
+</script>
+
+<style>
+
+</style>
