640572d3253a671ee6d3faefa38e3a688cb154d4
= <musah.abdulmalikgh@gmail.com>
2020-08-05 13:58:39 -1200

    Add delete category

--- src/admin/Categories.vue
+++ src/admin/Categories.vue
@@ -86,15 +86,22 @@
                                                                                     class="btn btn-success mr-2" 
                                                                                     type="button" data-toggle="modal" 
                                                                                     data-target="#exampleModal"
-                                                                                    @click="getId(category.id, key)">
-                                                                                        Add sub-category ({{category.subcategories.length}}) 
+                                                                                    @click="getId(category.id)">
+                                                                                        Add sub-category ({{category.subcategories ? category.subcategories.length : 0}}) 
                                                                                 </button>
                                                                                 <button class="btn btn-info mr-2" >View</button>
                                                                                 <button class="btn btn-danger mr-2"
                                                                                     type="button" data-toggle="modal" 
+                                                                                    data-target="#exampleModal1"
+                                                                                    @click="getId(category.id, key,category.name)"
+                                                                                   >Delete</button>
+                                                                                <button class="btn btn-primary"
+                                                                                    type="button" data-toggle="modal" 
                                                                                     data-target="#exampleModal"
-                                                                                    @click="getId(category.id, key)">Delete</button>
-                                                                                <button class="btn btn-primary">Update</button>
+                                                                                    @click="getId(category.id,key,category.name)"
+                                                                                    >
+                                                                                     Update
+                                                                                </button>
                                                                             </td>
                                                                         </tr>
                                                                     </tbody>
@@ -126,8 +133,11 @@
             <div class="modal-dialog">
               <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">
+                    <h5 class="modal-title" id="exampleModalLabel" v-if="!category_name">
                       Add SubCategory
+                    </h5>
+                    <h5 class="modal-title" id="exampleModalLabel" v-else-if="category_name">
+                      Add Update {{category_name}}
                     </h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
@@ -151,18 +161,26 @@
                         </div>
                     <form @submit.prevent="addSubCategory">
                         <div class="form-group">
-                            <label for="exampleInputEmail1">Add Category</label>
+                            <label for="exampleInputEmail1">Add Sub Category</label>
                                 <input type="text" class="form-control" v-model='category_name' id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Category Name" required>
                         </div>
                         <div class="">
                             <hr />
                             <button type="button" class="btn btn-danger mr-2 mybtn" data-dismiss="modal">Cancel</button>
-                            <button type="submit" class="btn btn-info">
+                            <button type="submit" class="btn btn-info" v-if="!category_name">
                                 Send
                                 <div class="spinner-grow" role="status" v-if="loading">
                                     <span class="sr-only">Loading...</span>
                                 </div>
-                        </button>
+                            </button>
+                            <button type="submit" class="btn btn-info"
+                                v-else-if="category_name"
+                                @click="updateCategory()">
+                                Update
+                                <div class="spinner-grow" role="status" v-if="loading">
+                                    <span class="sr-only">Loading...</span>
+                                </div>
+                            </button>
                         </div>
                     </form>
                 </div>
@@ -172,12 +190,49 @@
           </div>
           <!--- add new category ---> 
   </div>
+
+  <!--- start of delete modal --->
+          <div class="modal fade" id="exampleModal1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5>Delete Category</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                    <p v-if="!message"> Do you really want to delete {{category_name}}</p>
+                    <div class="col" v-else-if="deleteMessage">
+                            <div class="alert alert-warning">
+                                <p>
+                                    {{deleteMessage}}
+                                </p>
+                            </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-info mr-2 mybtn" @click="deleteCategory">
+                      Delete
+                        <div class="spinner-grow" role="status" v-if="loading">
+                            <span class="sr-only">Loading...</span>
+                        </div>
+                    </button>
+                  <button type="button" class="btn btn-danger mr-2 mybtn" data-dismiss="modal">Close</button>
+                </div>
+              </div>
+            </div>
+          </div>
   </div>
 </template> 
 
 <script>
 import axios from 'axios'
-
+function verifyToken(err) {
+    if(err.response.status === 401) {
+        this.$router.push('/admin_login')
+    }
+}
 //urrs
 const add_category = 'http://45.33.13.129:8001/api/category';
 
@@ -195,18 +250,24 @@ export default {
             get_category_error:null,
             category_id: null,
             category_key:null,
+            deleteMessage:null
         }
     },
     methods: {
-        getId(id, key) {
+        getId(id, key,categoryName) {
             this.category_id = id
             this.category_key = key
             this.add_category_error = null
+            this.category_name = categoryName ? categoryName : this.category_name
             this.message = null
+            this.deleteMessage = null
         }
         ,
         addCategory() {
             this.loading = true
+            this.add_category_error = null
+            this.message = null
+
             const data = {
                 'name': this.category_name,
             }
@@ -218,7 +279,7 @@ export default {
                 if(response.status == 201) {
                     this.loading = false,
                     this.message = 'Category Created Successfully'
-                    this.categories.push(response.data.catgory)
+                    this.categories.push(response.data.category)
                     this.category_name = null
                 }
             }).catch(err => {
@@ -226,6 +287,7 @@ export default {
                     this.loading = false;
                     this.add_category_error = "Network Error Try Again"
                 }
+                verifyToken(err)
             })
         },
         getCategories() {
@@ -234,7 +296,6 @@ export default {
                 .then(response => {
                     if(response.status === 200) {
                         this.categories = response.data.categories
-                        console.log('response', this.categories)
                     }
                 }).catch(err => {
                     if(err.request) {
@@ -259,14 +320,53 @@ export default {
                         this.loading = false
                         this.add_category_error = 'Network Error Try Again'
                     }
+                    verifyToken(err)
                 })
 
             },
+        deleteCategory(){
+            this.loading = true
+            axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
+            this.$http.delete(`http://45.33.13.129:8001/api/category/${this.category_id}`)
+                .then(response => {
+                    if(response.status === 200) {
+                        this.loading = false,
+                        this.deleteMessage = `${this.category_name} deleted successfully`
+                        this.categories.splice(this.category_key, 1)
+                    }
+                }).catch( err => {
+                    if(err.request) {
+                        this.loading = false
+                        this.deleteMessage = 'Network Error Try Again'
+                    }
+                    verifyToken(err)
+                })
+        },
+         updateCategory(){
+            this.loading = true
+            axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
+            this.$http.put(`http://45.33.13.129:8001/api/category/${this.category_id}`,{
+                    "name": this.category_name
+                })
+                .then(response => {
+                    if(response.status === 201) {
+                        this.loading = false,
+                        this.message = `${this.category_name} Update succesfully successfully`
+                        this.categories.splice(this.category_key, 1)
+                        this.getCategories.push(response.data.category)
+                    }
+                }).catch( err => {
+                    if(err.request) {
+                        this.loading = false
+                        this.add_category_error = 'Network Error Try Again'
+                    }
+                    verifyToken(err)
+                })
+        }
         
     },
     created(){
         this.getCategories();
-        console.log('data', this.categories)
     }
 }
 </script>
